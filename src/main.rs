9f47da86f060497610998133d240114f20a0fd1b
NeoTech <ptysiamaksim@gmail.com>
2026-04-02 08:26:54 +0200

    feat: sets image as a background

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::fs;
-use windows::core::{HSTRING, PCWSTR};
 use windows::Win32::UI::WindowsAndMessaging::{
-    SystemParametersInfoW, SPIF_SENDWININICHANGE, SPIF_UPDATEINIFILE, SPI_SETDESKWALLPAPER,
+    SPI_SETDESKWALLPAPER, SPIF_SENDWININICHANGE, SPIF_UPDATEINIFILE, SystemParametersInfoW,
 };
-use Window;
+use windows::core::{HSTRING};
 fn main() {
     let media_path = "media/wallpaper.jpg";
 
     match fs::canonicalize(&media_path) {
         Ok(absolute_path) => {
-            
+            let path_hstring = HSTRING::from(absolute_path.to_str().unwrap());
+
+            unsafe {
+                match SystemParametersInfoW(
+                    SPI_SETDESKWALLPAPER,
+                    0,
+                    Some(path_hstring.as_ptr() as *mut _),
+                    SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE,
+                ) {
+                    Ok(()) => println!("Wallpaper changed."),
+                    Err(_) => println!("Failed to change wallpaper.")
+                };
+            }
         }
         Err(_) => {
             eprintln!("No media found.");
         }
     }
-    
-    println!("Hello, world!");
-    Window::build();
 }
