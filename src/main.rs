ff881b74ec6f58055fb029e4f13b1a225d79abd3
NeoTech <ptysiamaksim@gmail.com>
2026-04-02 09:09:01 +0200

    feat: introduced minimal OOP for Wallpaper with all necessary structs, enums and functions

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
-use std::fs;
-use windows::Win32::UI::WindowsAndMessaging::{
-    SPI_SETDESKWALLPAPER, SPIF_SENDWININICHANGE, SPIF_UPDATEINIFILE, SystemParametersInfoW,
-};
-use windows::core::{HSTRING};
+use crate::wallpaper::wallpaper::{Wallpaper, WallpaperType};
+
+mod wallpaper;
 fn main() {
-    let media_path = "media/wallpaper.jpg";
+    let wallpaper: Wallpaper = Wallpaper {
+        name: "Example",
+        w_type: WallpaperType::Image,
+        path: "media/wallpaper.jpg"
+    };
 
-    match fs::canonicalize(&media_path) {
-        Ok(absolute_path) => {
-            let path_hstring = HSTRING::from(absolute_path.to_str().unwrap());
-
-            unsafe {
-                match SystemParametersInfoW(
-                    SPI_SETDESKWALLPAPER,
-                    0,
-                    Some(path_hstring.as_ptr() as *mut _),
-                    SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE,
-                ) {
-                    Ok(()) => println!("Wallpaper changed."),
-                    Err(_) => println!("Failed to change wallpaper.")
-                };
-            }
-        }
-        Err(_) => {
-            eprintln!("No media found.");
-        }
-    }
+    wallpaper.apply();
 }
